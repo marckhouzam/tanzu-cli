a28d4c0162c77672be7ba4a0838581994cfe5c8f
Marc Khouzam <kmarc@vmware.com>
2023-11-16 10:57:24 -0500

    explicit download
    Signed-off-by: Marc Khouzam <kmarc@vmware.com>

--- macDownload.sh
+++ macDownload.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-brew install vmware-tanzu/tanzu/tanzu-cli
+wget https://github.com/vmware-tanzu/tanzu-cli/releases/download/v1.1.0/tanzu-cli-darwin-arm64.tar.gz
+tar xf tanzu-cli-darwin-arm64.tar.gz --strip-components 1 v1.1.0/tanzu-cli-darwin_arm64
