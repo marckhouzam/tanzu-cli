93e9f62b6f094519b86dec9ea67a444f0067edfe
Marc Khouzam <marc.khouzam@broadcom.com>
2024-03-17 19:32:41 -0400

    Implement a yaml data store
    This can be used by the CLI to store non-configuration data.
    
    Signed-off-by: Marc Khouzam <marc.khouzam@broadcom.com>

---
+++ pkg/datastore/data_store.go
@@ -0,0 +1,160 @@
+// Copyright 2024 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// Package datastore implements the use of a data store yaml file
+// that can be used for the CLI to store and retrieve data that is not configuration.
+package datastore
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/adrg/xdg"
+	"github.com/pkg/errors"
+	"github.com/rogpeppe/go-internal/lockedfile"
+	"gopkg.in/yaml.v3"
+
+	"github.com/vmware-tanzu/tanzu-cli/pkg/utils"
+)
+
+// dataStoreFileName is the name of the data store yaml file
+// that is stored in the .config/tanzu directory.
+// It is a hidden file and should not be directly accessed by the user.
+const dataStoreFileName = ".data-store.yaml"
+
+var lockFile *lockedfile.File
+
+type DataStoreValue interface{}
+type DataStoreKey string
+
+type dataStoreContent map[DataStoreKey]DataStoreValue
+
+// GetDataStoreValue retrieves the value of the key from the data store.
+func GetDataStoreValue(key DataStoreKey) DataStoreValue {
+	content, err := getDataStoreContent(false)
+	if err != nil || content == nil {
+		return nil
+	}
+
+	return content[key]
+}
+
+// SetDataStoreValue sets the value of the key in the data store.
+func SetDataStoreValue(key DataStoreKey, value DataStoreValue) error {
+	content, err := getDataStoreContent(true)
+	if err != nil {
+		return err
+	}
+
+	if content == nil {
+		content = make(dataStoreContent)
+	}
+	content[key] = value
+
+	return saveAndClose(content)
+}
+
+// DeleteDataStoreValue deletes the key and value from the data store.
+// The previous value of the key is returned.
+func DeleteDataStoreValue(key DataStoreKey) (DataStoreValue, error) {
+	content, err := getDataStoreContent(true)
+	if err != nil {
+		return nil, err
+	}
+
+	deletedValue := content[key]
+	delete(content, key)
+
+	err = saveAndClose(content)
+	return deletedValue, err
+}
+
+// getDataStore retrieves the data store from the config directory along with locking the file.
+// If `setWriteLock` is false, it will read the data store file with a ReadLock and release the
+// lock at the same time.
+// If `setWriteLock` is true, it will apply a WriteLock to the data store file, read the file
+// and keep the WriteLock on the file.  The function saveAndClose() should be called to save
+// any changes and release the lock.
+func getDataStoreContent(setWriteLock bool) (dataStoreContent, error) {
+	var content dataStoreContent
+
+	b, err := getDataStoreBytes(setWriteLock)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return content, nil
+		}
+		return nil, err
+	}
+
+	err = yaml.Unmarshal(b, &content)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not decode data store file")
+	}
+
+	return content, nil
+}
+
+func getDataStoreBytes(setWriteLock bool) ([]byte, error) {
+	var err error
+	var b []byte
+
+	dsPath := getDataStorePath()
+	if setWriteLock {
+		dsDir := filepath.Dir(dsPath)
+		if !utils.PathExists(dsDir) {
+			// Create directory path if missing before locking the file
+			_ = os.MkdirAll(dsDir, 0755)
+		}
+		lockFile, err = lockedfile.Edit(dsPath)
+		if err != nil {
+			return nil, err
+		}
+
+		b, err = io.ReadAll(lockFile)
+	} else {
+		b, err = lockedfile.Read(dsPath)
+	}
+	return b, err
+}
+
+// getDataStorePath gets the data store file path
+func getDataStorePath() string {
+	// NOTE: TEST_CUSTOM_DATA_STORE_FILE is only for test purpose
+	customDSFile := os.Getenv("TEST_CUSTOM_DATA_STORE_FILE")
+	if customDSFile != "" {
+		return customDSFile
+	}
+
+	return filepath.Join(xdg.Home, ".config", "tanzu", dataStoreFileName)
+}
+
+// saveFile saves the data store file in the .config directory.
+func saveAndClose(content dataStoreContent) error {
+	if lockFile == nil {
+		return errors.New("cannot save the data store file as it is not locked")
+	}
+	defer lockFile.Close()
+
+	dsPath := getDataStorePath()
+	_, err := os.Stat(dsPath)
+	if err != nil {
+		return errors.Wrap(err, "could not stat the data store file")
+	}
+
+	out, err := yaml.Marshal(content)
+	if err != nil {
+		return errors.Wrap(err, "failed to encode the data store file")
+	}
+
+	if err := lockFile.Truncate(0); err != nil {
+		return errors.Wrap(err, "failed to truncate the data store file")
+	}
+	if _, err := lockFile.Seek(0, 0); err != nil {
+		return errors.Wrap(err, "failed to reset the data store file")
+	}
+	if _, err := lockFile.Write(out); err != nil {
+		return errors.Wrap(err, "failed to write the data store file")
+	}
+	return nil
+}
---
+++ pkg/datastore/data_store_test.go
@@ -0,0 +1,443 @@
+// Copyright 2024 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package datastore
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetDataStoreValue(t *testing.T) {
+	// Create a timestamp in the RFC3339 format
+	timestampStr := time.Now().Format(time.RFC3339)
+	// Now convert it back to a time.Time object so the two can be compared
+	timestamp, err := time.Parse(time.RFC3339, timestampStr)
+	assert.Nil(t, err)
+
+	tcs := []struct {
+		name      string
+		dsContent string
+		nofile    bool
+		nodir     bool
+		key       DataStoreKey
+		expected  DataStoreValue
+	}{
+		{
+			name:     "No directory for data store",
+			nodir:    true,
+			key:      "testKey",
+			expected: nil,
+		},
+		{
+			name:     "No file for data store",
+			nofile:   true,
+			key:      "testKey",
+			expected: nil,
+		},
+		{
+			name:      "Empty data store",
+			dsContent: "",
+			key:       "testKey",
+			expected:  nil,
+		},
+		{
+			name:      "String value",
+			dsContent: "testKey: testValue",
+			key:       "testKey",
+			expected:  "testValue",
+		},
+		{
+			name:      "Boolean true value",
+			dsContent: "testKey: true",
+			key:       "testKey",
+			expected:  true,
+		},
+		{
+			name:      "Boolean TRUE value",
+			dsContent: "testKey: TRUE",
+			key:       "testKey",
+			expected:  true,
+		},
+		{
+			name:      "Boolean FALSE value",
+			dsContent: "testKey: FALSE",
+			key:       "testKey",
+			expected:  false,
+		},
+		{
+			name:      "Boolean int value",
+			dsContent: "testKey: 1",
+			key:       "testKey",
+			expected:  1,
+		},
+		{
+			name:      "Timestamp value",
+			dsContent: "testKey: " + timestampStr,
+			key:       "testKey",
+			expected:  timestamp,
+		},
+		{
+			name: "Complex map value",
+			dsContent: `testKey:
+  testSubKey: testValue`,
+			key:      "testKey",
+			expected: map[string]interface{}{"testSubKey": "testValue"},
+		},
+		{
+			name: "More map of array value",
+			dsContent: `testKey:
+  testSubKey:
+  - testValue1
+  - testValue2`,
+			key:      "testKey",
+			expected: map[string]interface{}{"testSubKey": []interface{}{"testValue1", "testValue2"}},
+		},
+		{
+			name: "Missing key",
+			dsContent: `testKey:
+  testSubKey:
+  - testValue1
+  - testValue2`,
+			key:      "invalidKey",
+			expected: nil,
+		},
+		{
+			name: "Empty key",
+			dsContent: `testKey:
+  testSubKey:
+  - testValue1
+  - testValue2`,
+			key:      "",
+			expected: nil,
+		},
+		{
+			name:      "Empty value",
+			dsContent: "testKey: ",
+			key:       "testKey",
+			expected:  nil,
+		},
+	}
+
+	tmpDir, err := os.MkdirTemp("", "data_store_test")
+	assert.Nil(t, err)
+	assert.NotNil(t, tmpDir)
+	defer os.RemoveAll(tmpDir)
+
+	tmpDSFile, err := os.CreateTemp(tmpDir, "data-store.yaml")
+	assert.Nil(t, err)
+	assert.NotNil(t, tmpDSFile)
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.nodir {
+				// Set the environment variable to a nonexistent directory and file
+				nonExistentDir := tmpDir + "_nonexistentdir"
+				os.Setenv("TEST_CUSTOM_DATA_STORE_FILE", filepath.Join(nonExistentDir, "data-store.yaml"))
+				defer os.RemoveAll(nonExistentDir)
+			} else {
+				if tc.nofile {
+					// Set the environment variable to a nonexistent file
+					os.Setenv("TEST_CUSTOM_DATA_STORE_FILE", tmpDSFile.Name()+"_nonexistent")
+				} else {
+					// Write the data store test content to the file
+					err = os.WriteFile(tmpDSFile.Name(), []byte(tc.dsContent), 0644)
+					assert.Nil(t, err)
+					os.Setenv("TEST_CUSTOM_DATA_STORE_FILE", tmpDSFile.Name())
+				}
+			}
+
+			value := GetDataStoreValue(tc.key)
+			assert.Equal(t, tc.expected, value)
+		})
+	}
+	os.Unsetenv("TEST_CUSTOM_DATA_STORE_FILE")
+}
+
+func TestSetDataStoreValue(t *testing.T) {
+	// Create a timestamp in the RFC3339 format
+	timestampStr := time.Now().Format(time.RFC3339)
+	// Now convert it back to a time.Time object so the two can be compared
+	timestamp, err := time.Parse(time.RFC3339, timestampStr)
+	assert.Nil(t, err)
+
+	tcs := []struct {
+		name   string
+		nofile bool
+		nodir  bool
+		key    DataStoreKey
+		value  DataStoreValue
+		// expected is the expected value to be returned by GetDataStoreValue.
+		// Normally, it should be the same as value, but in some cases, it can be different.
+		expected DataStoreValue
+	}{
+		{
+			name:  "No directory for data store",
+			nodir: true,
+			key:   "testKey",
+			value: "testValue",
+		},
+		{
+			name:   "No file for data store",
+			nofile: true,
+			key:    "testKey",
+			value:  "testValue",
+		},
+		{
+			name:  "String value",
+			key:   "testKey",
+			value: "testValue",
+		},
+		{
+			name:  "Boolean true value",
+			key:   "testKey",
+			value: true,
+		},
+		{
+			name:  "Boolean TRUE value",
+			key:   "testKey",
+			value: true,
+		},
+		{
+			name:  "Boolean FALSE value",
+			key:   "testKey",
+			value: false,
+		},
+		{
+			name:  "Boolean int value",
+			key:   "testKey",
+			value: 1,
+		},
+		{
+			name:  "Timestamp value",
+			key:   "testKey",
+			value: timestamp,
+		},
+		{
+			name:     "Complex map value",
+			key:      "testKey",
+			value:    map[string]string{"testSubKey": "testValue"},
+			expected: map[string]interface{}{"testSubKey": "testValue"},
+		},
+		{
+			name:     "More map of array value",
+			key:      "testKey",
+			value:    map[string][]string{"testSubKey": {"testValue1", "testValue2"}},
+			expected: map[string]interface{}{"testSubKey": []interface{}{"testValue1", "testValue2"}},
+		},
+		{
+			name:  "Empty key",
+			key:   "",
+			value: "testValue",
+		},
+		{
+			name:  "Empty value",
+			key:   "testKey",
+			value: "",
+		},
+		{
+			name:  "Both empty",
+			key:   "",
+			value: "",
+		},
+	}
+
+	tmpDir, err := os.MkdirTemp("", "data_store_test")
+	assert.Nil(t, err)
+	assert.NotNil(t, tmpDir)
+	defer os.RemoveAll(tmpDir)
+
+	tmpDSFile, err := os.CreateTemp(tmpDir, "data-store.yaml")
+	assert.Nil(t, err)
+	assert.NotNil(t, tmpDSFile)
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.nodir {
+				// Set the environment variable to a nonexistent directory and file
+				nonExistentDir := tmpDir + "_nonexistentdir2"
+				os.Setenv("TEST_CUSTOM_DATA_STORE_FILE", filepath.Join(nonExistentDir, "data-store.yaml"))
+				defer os.RemoveAll(nonExistentDir)
+			} else {
+				if tc.nofile {
+					// Set the environment variable to a nonexistent file
+					os.Setenv("TEST_CUSTOM_DATA_STORE_FILE", tmpDSFile.Name()+"_nonexistent2")
+				} else {
+					// Set the environment variable to the file we already created
+					os.Setenv("TEST_CUSTOM_DATA_STORE_FILE", tmpDSFile.Name())
+				}
+			}
+
+			if tc.expected == nil {
+				tc.expected = tc.value
+			}
+
+			err = SetDataStoreValue(tc.key, tc.value)
+			assert.Nil(t, err)
+
+			value := GetDataStoreValue(tc.key)
+			assert.Equal(t, tc.expected, value)
+		})
+	}
+}
+
+func TestDeleteDataStoreValue(t *testing.T) {
+	// Create a timestamp in the RFC3339 format
+	timestampStr := time.Now().Format(time.RFC3339)
+	// Now convert it back to a time.Time object so the two can be compared
+	timestamp, err := time.Parse(time.RFC3339, timestampStr)
+	assert.Nil(t, err)
+
+	tcs := []struct {
+		name      string
+		dsContent string
+		nofile    bool
+		nodir     bool
+		key       DataStoreKey
+		expected  DataStoreValue
+	}{
+		{
+			name:     "No directory for data store",
+			nodir:    true,
+			key:      "testKey",
+			expected: nil,
+		},
+		{
+			name:     "No file for data store",
+			nofile:   true,
+			key:      "testKey",
+			expected: nil,
+		},
+		{
+			name:      "Empty data store",
+			dsContent: "",
+			key:       "testKey",
+			expected:  nil,
+		},
+		{
+			name:      "String value",
+			dsContent: "testKey: testValue",
+			key:       "testKey",
+			expected:  "testValue",
+		},
+		{
+			name:      "Boolean true value",
+			dsContent: "testKey: true",
+			key:       "testKey",
+			expected:  true,
+		},
+		{
+			name:      "Boolean TRUE value",
+			dsContent: "testKey: TRUE",
+			key:       "testKey",
+			expected:  true,
+		},
+		{
+			name:      "Boolean FALSE value",
+			dsContent: "testKey: FALSE",
+			key:       "testKey",
+			expected:  false,
+		},
+		{
+			name:      "Boolean int value",
+			dsContent: "testKey: 1",
+			key:       "testKey",
+			expected:  1,
+		},
+		{
+			name:      "Timestamp value",
+			dsContent: "testKey: " + timestampStr,
+			key:       "testKey",
+			expected:  timestamp,
+		},
+		{
+			name: "Complex map value",
+			dsContent: `testKey:
+  testSubKey: testValue`,
+			key:      "testKey",
+			expected: map[string]interface{}{"testSubKey": "testValue"},
+		},
+		{
+			name: "More map of array value",
+			dsContent: `testKey:
+  testSubKey:
+  - testValue1
+  - testValue2`,
+			key:      "testKey",
+			expected: map[string]interface{}{"testSubKey": []interface{}{"testValue1", "testValue2"}},
+		},
+		{
+			name: "Missing key",
+			dsContent: `testKey:
+  testSubKey:
+  - testValue1
+  - testValue2`,
+			key:      "invalidKey",
+			expected: nil,
+		},
+		{
+			name: "Empty key",
+			dsContent: `testKey:
+  testSubKey:
+  - testValue1
+  - testValue2`,
+			key:      "",
+			expected: nil,
+		},
+		{
+			name:      "Empty value",
+			dsContent: "testKey: ",
+			key:       "testKey",
+			expected:  nil,
+		},
+	}
+
+	tmpDir, err := os.MkdirTemp("", "data_store_test")
+	assert.Nil(t, err)
+	assert.NotNil(t, tmpDir)
+	defer os.RemoveAll(tmpDir)
+
+	tmpDSFile, err := os.CreateTemp(tmpDir, "data-store.yaml")
+	assert.Nil(t, err)
+	assert.NotNil(t, tmpDSFile)
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.nodir {
+				// Set the environment variable to a nonexistent directory and file
+				nonExistentDir := tmpDir + "_nonexistentdir3"
+				os.Setenv("TEST_CUSTOM_DATA_STORE_FILE", filepath.Join(nonExistentDir, "data-store.yaml"))
+				defer os.RemoveAll(nonExistentDir)
+			} else {
+				if tc.nofile {
+					// Set the environment variable to a nonexistent file
+					os.Setenv("TEST_CUSTOM_DATA_STORE_FILE", tmpDSFile.Name()+"_nonexistent3")
+				} else {
+					// Write the data store test content to the file
+					err = os.WriteFile(tmpDSFile.Name(), []byte(tc.dsContent), 0644)
+					assert.Nil(t, err)
+					os.Setenv("TEST_CUSTOM_DATA_STORE_FILE", tmpDSFile.Name())
+				}
+			}
+
+			value, err := DeleteDataStoreValue(tc.key)
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expected, value)
+
+			// Make sure the key is deleted
+			value = GetDataStoreValue(tc.key)
+			assert.Nil(t, value)
+		})
+	}
+	os.Unsetenv("TEST_CUSTOM_DATA_STORE_FILE")
+}
+
+func TestGetDataStorePath(t *testing.T) {
+	// Verify that the data store path is in the .config directory (not the .cache directory)
+	path := getDataStorePath()
+	assert.Contains(t, path, ".config")
+}
